3872af35879c91690351d806fd55be159e11cf4f
SeungSoo Oh <sunnyside74@gmail.com>
2020-10-18 14:50:58 +0900

    FIR 필터 처리 방법 테스트

--- pyOssDebug.py
+++ pyOssDebug.py
@@ -37,11 +37,41 @@ def dPlotAudio(audio_fs, data_plot, title_txt="title", label_txt="label", xl_txt
 
 	plot_time = np.linspace(start_time, end_time, data_plot.shape[0])
 	plt.title(title_txt)
-	plt.plot(plot_time, data_plot, label=label_txt)
+	fig = plt.plot(plot_time, data_plot, label=label_txt)
 	plt.legend()
 	plt.xlabel(xl_txt)
 	plt.ylabel(yl_txt)
 	plt.show()
+
+
+def dPlotAudio2(audio_fs, data_plot, title_txt="title", label_txt="label", xl_txt="x", yl_txt="y"):
+	"""
+    plot audio array using matplot for debug
+
+    Parameters
+    ----------
+	audio_fs: samplig frequency of audio file from audio fromat chunk
+    data_plot : data for plot
+	title_txt : , optional
+	label_txt : , optional
+	xl_txt : , optional
+	yl_txt : , optional
+
+    Returns
+    -------
+	"""
+	start_time = 0.
+	end_time = data_plot.shape[0] / audio_fs	# fs: audio_fmt_chunk[3]
+
+	plot_time = np.linspace(start_time, end_time, data_plot.shape[0])
+	plt.title(title_txt)
+	fig, ax = plt.subplots()
+	plot_time, data_plot, label=label_txt
+	plt.legend()
+	plt.xlabel(xl_txt)
+	plt.ylabel(yl_txt)
+	plt.show()
+
 
 def dPrint(func_name, dText, dData):
 	"""
