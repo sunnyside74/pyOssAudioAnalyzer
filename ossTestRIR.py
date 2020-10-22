ff8f7d3af62d6bc6011f947ae69a35ff258dd9c3
SeungSoo Oh <sunnyside74@gmail.com>
2020-10-22 21:28:28 +0900

    RIR Generator Python 코드 및 테스트 노트북 추가 (동작하지 않음)

--- ossTestRIR.py
+++ ossTestRIR.py
@@ -42,9 +42,9 @@ c = 340                         # Sound velocity (m/s)
 fs = 44100                      # Sample frequency (samples/s)
 r = [2, 1.5, 2]                 # Receiver position [x y z] (m)
 s = [2, 3.5, 2]                 # Source position [x y z] (m)
-L = [5, 4, 6]                     # Room dimensions [x y z] (m)
-beta = 0.4;                     # Reflections Coefficients
-n = 2048;                       # Number of samples
+L = [5, 4, 6]                   # Room dimensions [x y z] (m)
+beta = 0.4                      # Reflections Coefficients
+n = 2048                        # Number of samples
 mtype = 'omnidirectional'       # Type of microphone
 order = 2                       # Reflection order
 dim = 3                         # Room dimension
@@ -53,3 +53,4 @@ hp_filter = 1                   # Enable high-pass filter
 
 h2 = rir.generate_rir_ext(c, fs, r, s, L, beta, n, mtype, order, dim, orientation, hp_filter)
 
+print(h2)
