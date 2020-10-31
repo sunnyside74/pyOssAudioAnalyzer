dad3a049898c5e9db22fb800f0ebf20f84f4b4a1
SeungSoo Oh <sunnyside74@gmail.com>
2020-10-31 13:06:25 +0900

    JU Impulse 추가, pyroomacoustics 패키지 테스트

---
+++ pyOssGui.py
@@ -0,0 +1,44 @@
+'''
+# 라이브러리 정의
+ - 시스템 라이브러리
+ - 오디오 관련 라이브러리
+ - 연산 및 Plot 관련 라이브러리 
+ - 샤용자 라이브러리
+'''
+
+# Import Systems 
+import struct
+import io
+import os
+import sys
+import math
+import platform
+
+# Import Audio
+
+# Import GUI
+import tkinter as tk
+from tkinter import filedialog
+
+# User Libraries
+
+
+def ossTkFileOpen(pathname=os.getcwd(), title="Choose your file"):
+    init_dir = pathname
+
+    filename = tk.filedialog.askopenfilename(initialdir = init_dir,
+                                             title = title, 
+                                             filetypes = (("wave files","*.wav"),("npz files","*.npz"),("all files","*.*")))
+
+    print(filename)
+    return filename, path
+
+def ossTkFileSave():
+    init_dir = os.getcwd()
+    title = "Save your file"
+
+    filename = tk.filedialog.asksavefilename(initialdir = init_dir,
+                                             title = title, 
+                                             filetypes = (("wave files","*.wav"),("npz files","*.npz"),("all files","*.*")))
+    return filename
+
